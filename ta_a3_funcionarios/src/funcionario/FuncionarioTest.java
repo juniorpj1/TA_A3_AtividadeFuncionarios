68b3bb532fddc780cce16a101fb3f9da88b59d9d
apariciojunior <apariciojunior@MATRIZ011.rmp.com.br>
2023-03-31 16:40:49 -0300

    Criando o projeto, a classe Funcionario e FuncionarioTest. Implementando metodo para validar construtor.

---
+++ ta_a3_funcionarios/src/funcionario/FuncionarioTest.java
@@ -0,0 +1,15 @@
+package funcionario;
+
+import org.junit.jupiter.api.Test;
+
+public class FuncionarioTest {
+
+	@Test
+    public void testConstrutorFuncionario() {
+        Funcionario f = new Funcionario("João", 40, 60.0);
+        assertEquals("João", f.getNome());
+        assertEquals(40, f.getHorasTrabalhadas());
+        assertEquals(60.0, f.getValorHoraTrabalhada(), 0.001);
+    }
+	
+}
