6545c39fb17398312b5dcffaff1beb0d08ea44a1
apariciojunior <apariciojunior@MATRIZ011.rmp.com.br>
2023-03-31 16:50:11 -0300

    implementando testes de validacao de construtor e getters

--- ta_a3_funcionarios/src/funcionario/Funcionario.java
+++ ta_a3_funcionarios/src/funcionario/Funcionario.java
@@ -2,8 +2,49 @@ package funcionario;
 
 public class Funcionario {
 
-	public Funcionario(String string, int i, double d) {
-		// TODO Auto-generated constructor stub
+	private String nome;
+	private int horasTrabalhadas;
+	private double valorHoraTrabalhada;
+
+	public Funcionario(String nome, int horasTrabalhadas, double valorHora) {
+		this.nome = nome;
+		this.horasTrabalhadas = validaHorasTrabalhadas(horasTrabalhadas);
+		this.valorHoraTrabalhada = validaValorHoraTrabalhada(valorHoraTrabalhada);
+	}
+
+	public void setHorasTrabalhadas(int horasTrabalhadas) {
+		this.horasTrabalhadas = validaHorasTrabalhadas(horasTrabalhadas);
+	}
+
+	private int validaHorasTrabalhadas(int horasTrabalhadas) {
+		if (horasTrabalhadas > 40) {
+			throw new IllegalArgumentException(
+					"O número de horas trabalhadas por funcionários próprios deve ser menor ou igual a 40.");
+		}
+		return horasTrabalhadas;
+	}
+
+	private double validaValorHoraTrabalhada(double valorHoraTrabalhada) {
+		double valorMinimo = 1320.0 * 0.04;
+		double valorMaximo = 1320.0 * 0.1;
+		if (valorHoraTrabalhada < valorMinimo || valorHoraTrabalhada > valorMaximo) {
+			throw new IllegalArgumentException(
+					String.format("Valor hora trabalhada deve estar entre %.2f e %.2f.", valorMinimo, valorMaximo));
+		}
+		return valorHoraTrabalhada;
+
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public int getHorasTrabalhadas() {
+		return this.horasTrabalhadas;
+	}
+
+	public double getValorHoraTrabalhada() {
+		return this.valorHoraTrabalhada;
 	}
 
 }
